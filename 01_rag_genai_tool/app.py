b6e7cef5327222b451c90b86a1e9f931e11fe5cd
Taral Sanjay Sarvagod <taralwithmac@Tarals-MacBook-Air.local>
2026-05-17 13:54:49 +0200

    fix: update text splitter import

--- 01_rag_genai_tool/app.py
+++ 01_rag_genai_tool/app.py
@@ -5,7 +5,7 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.prompts import PromptTemplate
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 
